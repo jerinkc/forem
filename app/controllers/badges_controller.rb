1dc4cc045d68043c80708cc7504af8c69118f65d
Jerinkc <jerinkc22@gmail.com>
2023-12-12 00:32:40 +0530

    Modify badges controller

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,14 +1,9 @@
 class BadgesController < ApplicationController
-  before_action :set_cache_control_headers, only: [:show]
-  # No authorization required for entirely public controller
-
   def index
-    @badges = Badge.order(:created_at)
-    @earned_badge_achievements = user_signed_in? ? current_user.badge_achievements.pluck(:badge_id) : []
-  end
-
-  def show
-    @badge = Badge.find_by(slug: params[:slug]) || not_found
-    set_surrogate_key_header "badges-show-action"
+    @badges = Badge.order(created_at: :desc)
+      .to_json(only: %i[title slug description badge_image created_at badge_category_id])
+    @badge_categories = BadgeCategory.order(:name)
+      .where.not(badges: nil)
+      .to_json(only: %i[id name description])
   end
 end
